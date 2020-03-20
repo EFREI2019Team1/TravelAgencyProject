98b2af1a974ab5a57ffd2bd875a4bf1c1d8235ad
uma110 <giorno_haruno@yahoo.co.jp>
2020-03-20 16:40:13 +0900

    予約システムの完成 グーグルアカウントでのログイン アカウントへのデータベース（予約状況）の紐づけ 個人ページでの予約情報の取り出し、HTMLへの反映
    未完成
    データベースのセキュリティルール

--- public/booking_page/bookingSystem.js
+++ public/booking_page/bookingSystem.js
@@ -1,3 +1,4 @@
+'use strict';
 $(function () {
     const login = $('#login');
     const logout = $('#logout');
@@ -32,17 +33,99 @@ $(function () {
     })
 
     $("#book_button").click(function () {
-        console.log("aa");
-        let db = firebase.firestore();
-        db.collection("courses").doc('111').set(
-            {
-                data1:"unchi1",
-            }
-        )
-        db.collection("courses").add(
-            {
-                data2:"unchi2",
-            }
-        )
+        reserveCourseOnDB(courseList[0].getID);
+        addCourseOnDisp(courseList[0].name,courseList[0].getID);
     });
+
+    $("#delete_button").click(() => {
+        deleteReservedCourseOnDB(courseList[0].getID);
+        deleteCOurseOnDisp(courseList[0].getID);
+    })
+
+    $("#zikken").click(() => {
+        updateReservedCourseDisp();
+        // deleteCOurseOnDisp(courseList[0].getID);
+        // updateReservedCourseDisp();
+    })
 })
+
+//add a course to display(ul li)
+function addCourseOnDisp(name, uid) {
+    let $bookList = $("#bookCourseList");
+
+    let $label = $("<label>", {
+        class: "reserved_course_name",
+    }).text(name);
+
+    //データタグの使い方、jqueryとの違い
+    $label.data('uid', uid);
+
+    let $list = $('<li>').append($label);
+
+    $bookList.append($list);
+}
+
+//delete a course from display(ul li)
+function deleteCOurseOnDisp(course_uid) {
+    let $reservedCourseLists = $("#bookCourseList > li >label");
+    $.each($reservedCourseLists, function (indexInArray, elem) {
+        let reservedCourseID = $(elem).data('uid');
+        if (reservedCourseID === course_uid) {
+            $(elem).parent().remove();
+        }
+    });
+}
+
+//update display reserved course(get from database and make reserved course)
+function updateReservedCourseDisp() {
+    let myuser = firebase.auth().currentUser;
+    if (myuser) {
+        $("#bookCourseList").empty();
+
+        let db = firebase.firestore();
+
+        let reservedCourseIDList = new Array();
+        db.collection('clients').doc(myuser.uid).get().then(clientDoc => {
+            let data = clientDoc.data();
+            reservedCourseIDList = data.booking_courseid;
+        }).finally(() => {
+            courseList.forEach(course => {
+                let isMatchedID = false;
+                reservedCourseIDList.forEach(targetID => {
+                    if (course.getID === targetID) isMatchedID = true;
+                })
+                if (isMatchedID) {
+                    addCourseOnDisp(course.name, course.getID);
+                }
+            })
+        })
+    } else {
+        alert("Please login your account");
+    }
+}
+
+//database operation
+function deleteReservedCourseOnDB(courseID) {
+    let myuser = firebase.auth().currentUser;
+    if (myuser) {
+        let db = firebase.firestore();
+        db.collection('clients').doc(myuser.uid).update({
+            booking_courseid: firebase.firestore.FieldValue.arrayRemove(courseList[0].getID)
+        })
+    } else {
+        alert("Please login your account in order to remove travel course");
+    }
+}
+
+//database operation
+function reserveCourseOnDB(courseID) {
+    let myuser = firebase.auth().currentUser;
+    if (myuser) {
+        let db = firebase.firestore();
+        db.collection("clients").doc(myuser.uid).update({
+            booking_courseid: firebase.firestore.FieldValue.arrayUnion(courseID)
+        })
+    } else {
+        alert("Please login your account in order to book travel course");
+    }
+}
--- public/data.js
+++ public/data.js
@@ -24,13 +24,17 @@ var CONTENT = {
 var courseList = new Array();
 //コースクラス
 class Course {
-    constructor(_name, _day, _target, _pathToImage, _pathToHTMLfile, _moneyLimit, _contentTag) {
-        this._id = getUniqueID();
+    constructor(_id, _name, _day, _target, _pathToImage, _moneyLimit, _contentTag) {
+        if (_id) {
+            this._id = _id;
+        } else {
+            this._id = getUniqueID();
+
+        }
         this._name = _name;
         this._day = _day;
         this._target = _target;
         this._pathToImage = _pathToImage;
-        this._pathToHTMLfile = _pathToHTMLfile;
         this._moneyLimit = _moneyLimit;
         this._contentTag = _contentTag;
         courseList.push(this);
@@ -63,12 +67,6 @@ class Course {
     get pathToImage() {
         return this._pathToImage;
     }
-    set pathToHTMLfile(value) {
-        this._pathToHTMLfile = value;
-    }
-    get pathToHTMLfile() {
-        return this._pathToHTMLfile;
-    }
     set moneyLimit(value) {
         this._moneyLimit = value;
     }
@@ -83,25 +81,65 @@ class Course {
     }
 }
 
-$(function(){
-    let course1 = new Course("コッツウォルズ地方とロンドン", 7, TARGET.ADULT, "UI/course01.jpg", null, 289900, [CONTENT.WORLD_HERITAGE, CONTENT.MUSEUM, CONTENT.SIMBOL]);
-    let course2 = new Course("最低価格！1weekぶらり旅", 7, TARGET.YOUNG, "UI/course02.jpg", null, 30000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
-    let course3 = new Course("ロンドン観光、一週間の旅", 7, TARGET.YOUNG, "UI/course03.jpg", null, 100000, [CONTENT.SHOPPING, CONTENT.MUSEUM, CONTENT.SIMBOL, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
-    let course4 = new Course("ロンドン建築巡り！", 2, TARGET.COUPLE, "UI/course04.jpg", null, 20000, [CONTENT.WORLD_HERITAGE, CONTENT.SIMBOL, CONTENT.HISTORIC, CONTENT.INSTAGRAM]);
-    let course5 = new Course("最低価格！3daysぶらり旅", 3, TARGET.YOUNG, "UI/course05.jpg", null, 10000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
-    let course6 = new Course("最低価格！2daysぶらり旅", 2, TARGET.YOUNG, "UI/course06.jpg", null, 5000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
-    let course7 = new Course("ロンドン名所巡り", 2, TARGET.YOUNG, "UI/course07.jpg", null, 10000, [CONTENT.WORLD_HERITAGE, CONTENT.SIMBOL, CONTENT.PARK, CONTENT.HISTORIC]);
-    let course8 = new Course("街歩きとミュージカル鑑賞", 2, TARGET.FAMILY, "UI/course08.jpg", null, 15000, [CONTENT.SHOPPING, CONTENT.SHOW, CONTENT.HISTORIC]);
-    let course9 = new Course("クルーズ&バス", 2, TARGET.ADULT, "UI/course09.jpg", null, 15000, [CONTENT.WORLD_HERITAGE, CONTENT.SHOPPING, CONTENT.SIMBOL, CONTENT.PARK, CONTENT.HISTORIC]);
-    let course10 = new Course("ハリーポッターとシャーロックホームズ/名作のロケ地巡り", 2, TARGET.FAMILY, "UI/course10.jpg", null, 25000, [CONTENT.SHOW, CONTENT.FILMING_LOCATION]);
-    let course11 = new Course("ファミリーツアー", 7, TARGET.FAMILY, "UI/course11.jpg", null, 270000, [CONTENT.SHOPPING, CONTENT.PARK, CONTENT.HISTORIC, CONTENT.INSTAGRAM]);
-    let course12 = new Course("3days for family", 3, TARGET.FAMILY, "UI/course12.jpg", null, 100000, [CONTENT.SHOPPING, CONTENT.PARK, CONTENT.FILMING_LOCATION]);
-    let course13 = new Course("2人でまったり絶景巡り", 2, TARGET.COUPLE, "UI/course13.jpg", null, 20000, [CONTENT.SIMBOL, CONTENT.INSTAGRAM]);
-    let course14 = new Course("3days for couple", 3, TARGET.COUPLE, "UI/course14.jpg", null, 100000, [CONTENT.WORLD_HERITAGE, CONTENT.HISTORIC, CONTENT.FILMING_LOCATION]);
-    let course15 = new Course("まったり博物館・美術館巡り", 7, TARGET.ADULT, "UI/course15.jpg", null, 70000, [CONTENT.WORLD_HERITAGE, CONTENT.MUSEUM, CONTENT.SIMBOL, CONTENT.HISTORIC]);
-    let course16 = new Course("3 days for adults(優雅)", 3, TARGET.ADULT, "UI/course16.jpg", null, 100000, [CONTENT.WORLD_HERITAGE, CONTENT.TEA_TIME, CONTENT.SHOW, CONTENT.HISTORIC]);
-    let course17 = new Course("shopping and theatre", 2, TARGET.FAMILY, "UI/course17.jpg", null, 15000, [CONTENT.SHOPPING, CONTENT.FASHION, CONTENT.SHOW]);
-    let course18 = new Course("ロンドンのおすすめ観光スポット15選！", 3, TARGET.COUPLE, "UI/course18.jpg", null, 20000, [CONTENT.SIMBOL, CONTENT.HISTORIC, CONTENT.INSTAGRAM, CONTENT.FILMING_LOCATION]);
-    let course19 = new Course("3 days with musical", 3, TARGET.FAMILY, "UI/course19.jpg", null, 100000, [CONTENT.SHOPPING, CONTENT.MUSEUM, CONTENT.SHOW]);
-    let course20 = new Course("まるまる3日間　ロンドンを歩き回る", 3, TARGET.ADULT, "UI/course20.jpg", null, 36000, [CONTENT.SHOPPING, CONTENT.SHOW]);
+$(function () {
+    loadCourseDataOnDB();
 })
+
+// course data
+/*
+{
+    let course1 = new Course("コッツウォルズ地方とロンドン", 7, TARGET.ADULT, "UI/course01.jpg", 289900, [CONTENT.WORLD_HERITAGE, CONTENT.MUSEUM, CONTENT.SIMBOL]);
+    let course2 = new Course("最低価格！1weekぶらり旅", 7, TARGET.YOUNG, "UI/course02.jpg", 30000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
+    let course3 = new Course("ロンドン観光、一週間の旅", 7, TARGET.YOUNG, "UI/course03.jpg", 100000, [CONTENT.SHOPPING, CONTENT.MUSEUM, CONTENT.SIMBOL, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
+    let course4 = new Course("ロンドン建築巡り！", 2, TARGET.COUPLE, "UI/course04.jpg", 20000, [CONTENT.WORLD_HERITAGE, CONTENT.SIMBOL, CONTENT.HISTORIC, CONTENT.INSTAGRAM]);
+    let course5 = new Course("最低価格！3daysぶらり旅", 3, TARGET.YOUNG, "UI/course05.jpg", 10000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
+    let course6 = new Course("最低価格！2daysぶらり旅", 2, TARGET.YOUNG, "UI/course06.jpg", 5000, [CONTENT.SHOPPING, CONTENT.IVENT, CONTENT.FASHION, CONTENT.TEA_TIME, CONTENT.INSTAGRAM]);
+    let course7 = new Course("ロンドン名所巡り", 2, TARGET.YOUNG, "UI/course07.jpg", 10000, [CONTENT.WORLD_HERITAGE, CONTENT.SIMBOL, CONTENT.PARK, CONTENT.HISTORIC]);
+    let course8 = new Course("街歩きとミュージカル鑑賞", 2, TARGET.FAMILY, "UI/course08.jpg", 15000, [CONTENT.SHOPPING, CONTENT.SHOW, CONTENT.HISTORIC]);
+    let course9 = new Course("クルーズ&バス", 2, TARGET.ADULT, "UI/course09.jpg", 15000, [CONTENT.WORLD_HERITAGE, CONTENT.SHOPPING, CONTENT.SIMBOL, CONTENT.PARK, CONTENT.HISTORIC]);
+    let course10 = new Course("ハリーポッターとシャーロックホームズ/名作のロケ地巡り", 2, TARGET.FAMILY, "UI/course10.jpg", 25000, [CONTENT.SHOW, CONTENT.FILMING_LOCATION]);
+    let course11 = new Course("ファミリーツアー", 7, TARGET.FAMILY, "UI/course11.jpg", 270000, [CONTENT.SHOPPING, CONTENT.PARK, CONTENT.HISTORIC, CONTENT.INSTAGRAM]);
+    let course12 = new Course("3days for family", 3, TARGET.FAMILY, "UI/course12.jpg", 100000, [CONTENT.SHOPPING, CONTENT.PARK, CONTENT.FILMING_LOCATION]);
+    let course13 = new Course("2人でまったり絶景巡り", 2, TARGET.COUPLE, "UI/course13.jpg", 20000, [CONTENT.SIMBOL, CONTENT.INSTAGRAM]);
+    let course14 = new Course("3days for couple", 3, TARGET.COUPLE, "UI/course14.jpg", 100000, [CONTENT.WORLD_HERITAGE, CONTENT.HISTORIC, CONTENT.FILMING_LOCATION]);
+    let course15 = new Course("まったり博物館・美術館巡り", 7, TARGET.ADULT, "UI/course15.jpg", 70000, [CONTENT.WORLD_HERITAGE, CONTENT.MUSEUM, CONTENT.SIMBOL, CONTENT.HISTORIC]);
+    let course16 = new Course("3 days for adults(優雅)", 3, TARGET.ADULT, "UI/course16.jpg", 100000, [CONTENT.WORLD_HERITAGE, CONTENT.TEA_TIME, CONTENT.SHOW, CONTENT.HISTORIC]);
+    let course17 = new Course("shopping and theatre", 2, TARGET.FAMILY, "UI/course17.jpg", 15000, [CONTENT.SHOPPING, CONTENT.FASHION, CONTENT.SHOW]);
+    let course18 = new Course("ロンドンのおすすめ観光スポット15選！", 3, TARGET.COUPLE, "UI/course18.jpg", 20000, [CONTENT.SIMBOL, CONTENT.HISTORIC, CONTENT.INSTAGRAM, CONTENT.FILMING_LOCATION]);
+    let course19 = new Course("3 days with musical", 3, TARGET.FAMILY, "UI/course19.jpg", 100000, [CONTENT.SHOPPING, CONTENT.MUSEUM, CONTENT.SHOW]);
+    let course20 = new Course("まるまる3日間　ロンドンを歩き回る", 3, TARGET.ADULT, "UI/course20.jpg", 36000, [CONTENT.SHOPPING, CONTENT.SHOW]);
+
+}
+*/
+
+function saveCourseDataOnDB() {
+    let db = firebase.firestore();
+    courseList.forEach(course => {
+        db.collection("courses").add(
+            {
+                name: course.name,
+                day: course.day,
+                target: course.target,
+                pathToImage: course.pathToImage,
+                moneyLimit: course.moneyLimit,
+                contentTag: course.contentTag
+            }
+        )
+    })
+}
+
+function loadCourseDataOnDB() {
+    let db = firebase.firestore();
+    let tmp = courseList;
+    courseList = new Array();
+    db.collection("courses").get().then(courseDocs => {
+        courseDocs.forEach(doc => {
+            let courseData = doc.data();
+            let course = new Course(doc.id, courseData.name, courseData.day,
+                courseData.target, courseData.pathToImage,
+                courseData.moneyLimit, courseData.contentTag);
+        })
+    }).catch(error => {
+        console.log("database get error => " + error);
+    })
+}
